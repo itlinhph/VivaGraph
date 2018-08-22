64db87f6187c2ebab64539f44ed5e2c654bd7ba0
linkpp <linhphanhust@gmail.com>
2018-08-22 14:31:31 +0700

    front-end main node

---
+++ resource/script/script.js
@@ -0,0 +1,134 @@
+
+     
+    // Load Data
+    var nodeList = []; 
+    
+    
+    var egdeList = []; // private information
+
+    // console.log(egdeList);
+    // console.log(nodeList);
+    // creat graph
+    var graph = Viva.Graph.graph();
+    nodeList.forEach(function(item) {
+        if(item[2] == "kho_tong")
+            // graph.addNode(item[0], item[1]).isPinned = true ;
+            graph.addNode(item[0], {name: item[1], type: item[2], vido: item[3], kinhdo: item[4]});
+        // else
+        //     graph.addNode(item[0], item[1]) ;
+        
+    });
+
+    egdeList.forEach(function(item){
+        graph.addLink(item[0], item[1], item[2]) ;
+    });
+
+    
+    // customize node
+    var graphics = Viva.Graph.View.svgGraphics(),
+        nodeSize = 20,
+        hightlightRelateNodes = function(nodeId, isOn) {
+            graph.forEachLinkedNode(nodeId, function(node, link) {
+                var isFly = (link.data == "fly");
+                var baseColor = "blue" ;
+                if(isFly)
+                    baseColor = "red";
+                var linkUI = graphics.getLinkUI(link.id) ;
+                if(linkUI) {
+                    linkUI.attr("stroke", isOn ? "yellow" : baseColor);
+                }
+            });
+        };
+
+    graphics.node(function(node) {
+        var ui  =  Viva.Graph.svg('g'),
+        stationName = Viva.Graph.svg('text').attr('y', '-5px').text(node.id+" "+node.data.name),
+        img = Viva.Graph.svg('image')     
+            .attr('width', nodeSize)
+            .attr('height', nodeSize)
+            .link('resource/img/ghtk.png');
+        ui.append(stationName) ;
+        ui.append(img);
+        
+        $(ui).hover(function(){
+            hightlightRelateNodes(node.id, true);
+        }, function() {
+            hightlightRelateNodes(node.id, false) ;
+        });
+        return ui;
+    }).placeNode(function(nodeUI, pos) {
+        nodeUI.attr('transform',
+            'translate(' +
+            (pos.x - nodeSize / 2) + ',' + (pos.y - nodeSize / 2) +
+            ')');
+    });
+    
+    // creat Marker direct link
+    var createMarker = function(id) {
+            return Viva.Graph.svg('marker')
+                        .attr('id', id)
+                        .attr('viewBox', "0 0 10 10")
+                        .attr('refX', "10")
+                        .attr('refY', "5")
+                        .attr('markerUnits', "strokeWidth")
+                        .attr('markerWidth', "10")
+                        .attr('markerHeight', "5")
+                        .attr('orient', "auto");
+        },
+
+        marker = createMarker('Triangle');
+
+    marker.append('path').attr('d', 'M 0 0 L 10 5 L 0 10 z');
+
+    var defs = graphics.getSvgRoot().append('defs');
+    defs.append(marker);
+
+    var geom = Viva.Graph.geom();
+    
+    // Custom link
+    graphics.link(function(link){
+        var isFly = (link.data == 'fly'),
+            
+        uil = Viva.Graph.svg('path')
+                    .attr('stroke', isFly ? 'red' : 'blue')
+                    .attr('fill', 'none')
+                    .attr('marker-end', 'url(#Triangle)');
+        uil.isFly = isFly ;
+        return uil;
+    }).placeLink(function(linkUI, fromPos, toPos) {
+        var toNodeSize = nodeSize,
+            fromNodeSize = nodeSize;
+
+        var from = geom.intersectRect(
+                // rectangle:
+                        fromPos.x - fromNodeSize / 2, // left
+                        fromPos.y - fromNodeSize / 2, // top
+                        fromPos.x + fromNodeSize / 2, // right
+                        fromPos.y + fromNodeSize / 2, // bottom
+                // segment:
+                        fromPos.x, fromPos.y, toPos.x, toPos.y) || fromPos;
+
+        var to = geom.intersectRect(
+                // rectangle:
+                        toPos.x - toNodeSize / 2, // left
+                        toPos.y - toNodeSize / 2, // top
+                        toPos.x + toNodeSize / 2, // right
+                        toPos.y + toNodeSize / 2, // bottom
+                // segment:
+                        toPos.x, toPos.y, fromPos.x, fromPos.y) || toPos; 
+        
+        var ry = linkUI.isFly ? 5 : 0,
+        data = 'M' + from.x + ',' + from.y +
+                //    'L' + to.x + ',' + to.y;
+                    ' A 5,' + ry + ',-10,0,1,' + to.x + ',' + to.y;
+        linkUI.attr('d', data);
+    });
+
+    // Render the graph
+    var renderer = Viva.Graph.View.renderer(graph, {
+        graphics : graphics
+    }) ;
+            
+    function renderGraph() {
+        renderer.run();
+    }
